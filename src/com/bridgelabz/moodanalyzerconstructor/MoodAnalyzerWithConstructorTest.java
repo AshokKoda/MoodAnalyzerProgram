4b23616ff8f4ea812bc090cc8f92c6fe6c392123
AshokKoda <a.koda59@gmail.com>
2022-04-14 16:10:50 +0530

    Added:RepeatedTC1.2 - Given message in constructor should return HAPPY

--- src/com/bridgelabz/moodanalyzerconstructor/MoodAnalyzerWithConstructorTest.java
+++ src/com/bridgelabz/moodanalyzerconstructor/MoodAnalyzerWithConstructorTest.java
@@ -12,4 +12,12 @@ public class MoodAnalyzerWithConstructorTest {
 		System.out.println("Iam in " + result + " mood.");
 		Assert.assertEquals("SAD", result);
 	}
+	
+	@Test
+	public void respondAsHappyMoodWithConstructor() { // Repeated TC1.2
+		MoodAnalyzerWithConstructor moodAnalyzer = new MoodAnalyzerWithConstructor();
+		String result = moodAnalyzer.analyseMood("I am in happy Mood"); // Here using Parameterized Constructor 
+		System.out.println("Iam in " + result + " mood.");
+		Assert.assertEquals("HAPPY", result);
+	}
 }
