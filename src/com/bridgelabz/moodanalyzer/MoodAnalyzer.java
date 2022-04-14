1871d2f178bb432f9ba4b19326b807e6c39d29cf
AshokKoda <a.koda59@gmail.com>
2022-04-14 15:21:17 +0530

    Added:UC1 - Respond happy or sad mood

--- src/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
+++ src/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
@@ -2,8 +2,24 @@ package com.bridgelabz.moodanalyzer;
 
 public class MoodAnalyzer {
 
+	public String moodAnalysis(String msg) {
+
+		if (msg.contains("sad")) {
+			return "Sad";
+		} else {
+			return "Happy";
+		}
+	}
+
 	public static void main(String[] args) {
 		System.out.println("********Welcom To Mood Analyzer Problems***********");
+		
+		MoodAnalyzer moodAnalysis = new MoodAnalyzer();
+		String sadMood = moodAnalysis.moodAnalysis("Iam sad mood");
+		System.out.println("My Mood is: " + sadMood);
+		System.out.println("<----------------OR------------------->");
+		String happyMood = moodAnalysis.moodAnalysis("Iam happy mood");
+		System.out.println("My Mood is: " + happyMood);
 
 	}
 
