ca792e89b0912d94b20db702bb868e72319bffbe
AshokKoda <a.koda59@gmail.com>
2022-04-14 15:28:54 +0530

    Added:TC1.1 - Give message should return SAD

---
+++ src/com/bridgelabz/moodanalyzer/MoodAnalyzerTest.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.moodanalyzer;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyzerTest {
+
+	@Test
+	public void respondAsSadMood() { // TC1.1
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+		String result = moodAnalyzer.moodAnalysis("I am in sad Mood");
+		System.out.println("Iam in " + result + " mood.");
+		Assert.assertEquals("Sad", result);
+	}
+}
