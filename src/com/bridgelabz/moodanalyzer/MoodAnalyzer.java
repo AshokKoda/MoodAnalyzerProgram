15aa24c5fe7c97246771d23952dfffbe53e1b47c
AshokKoda <a.koda59@gmail.com>
2022-04-14 15:12:48 +0530

    Added:Welcome to Mood Analyzer

---
+++ src/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.moodanalyzer;
+
+public class MoodAnalyzer {
+
+	public static void main(String[] args) {
+		System.out.println("********Welcom To Mood Analyzer Problems***********");
+
+	}
+
+}
