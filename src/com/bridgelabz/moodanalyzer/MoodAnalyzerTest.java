412043af7f8fbf8ccb50a16a5f706eeecadf8a34
AshokKoda <a.koda59@gmail.com>
2022-04-14 15:36:57 +0530

    Added:TC1.2 - Give message should return HAPPY

--- src/com/bridgelabz/moodanalyzer/MoodAnalyzerTest.java
+++ src/com/bridgelabz/moodanalyzer/MoodAnalyzerTest.java
@@ -12,4 +12,12 @@ public class MoodAnalyzerTest {
 		System.out.println("Iam in " + result + " mood.");
 		Assert.assertEquals("Sad", result);
 	}
+	
+	@Test
+	public void respondAsHappyMood() { // TC1.2
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+		String result = moodAnalyzer.moodAnalysis("I am in happy Mood");
+		System.out.println("Iam in " + result + " mood.");
+		Assert.assertEquals("Happy", result);
+	}
 }
